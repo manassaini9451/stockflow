983bbf4dd1dff4dfc79eccd19a0e707fa156071a
unknown <manassaini.9451@gmail.com>
2026-03-27 15:17:41 +0530

    server issue fixed

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -3,17 +3,25 @@ const jwt = require("jsonwebtoken");
 const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization;
 
-  if (!token) {
+   if (!token) {
     return res.status(401).json({ message: "No token provided" });
   }
 
   try {
+    // 🔥 IMPORTANT FIX
+    if (token.startsWith("Bearer")) {
+      token = token.split(" ")[1]; // remove "Bearer"
+    }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     req.user = decoded;
     next();
   } catch (err) {
-    res.status(401).json({ message: "Invalid token" });
+    console.log("JWT ERROR:", err.message); // debug
+    return res.status(401).json({ message: "Invalid token" });
   }
 };
+
 
 module.exports = authMiddleware;
