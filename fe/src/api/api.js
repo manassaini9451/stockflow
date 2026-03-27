f298c9ca23a5fccdfb85cb47347c329594cfc2ef
unknown <manassaini.9451@gmail.com>
2026-03-27 14:51:41 +0530

    api url change

--- fe/src/api/api.js
+++ fe/src/api/api.js
@@ -1,16 +1,35 @@
 import axios from "axios";
 
+// Base URL from environment variable
 const API = axios.create({
-  baseURL: "https://stockflow-2.onrender.com/api"
+  baseURL: `${process.env.REACT_APP_API_URL}/api`
 });
 
-// attach token
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = token;
+// Request interceptor (attach token)
+API.interceptors.request.use(
+  (req) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      req.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return req;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return req;
-});
+);
+
+// Response interceptor (optional but pro level)
+API.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.log("Unauthorized, please login again");
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default API;
